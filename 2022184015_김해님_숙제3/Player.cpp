1e4fd2c3874e0f5f3d4c2ba15d4ff2282b11d7b1
D4LGONA(asus) <laff3y2297@gmail.com>
2025-06-20 03:31:53 +0900

    끝.
    설명문서 적는 중... 느낀점만 적으면 됨

--- 2022184015_김해님_숙제3/Player.cpp
+++ 2022184015_김해님_숙제3/Player.cpp
@@ -249,7 +249,7 @@ CTerrainPlayer::CTerrainPlayer(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandLi
 
 	CHeightMapTerrain* pTerrain = (CHeightMapTerrain*)pContext; 
 	float fHeight = pTerrain->GetHeight(pTerrain->GetWidth() * 0.5f, pTerrain->GetLength() * 0.5f);
-	SetPosition(XMFLOAT3(pTerrain->GetWidth() * 0.5f, fHeight + 100.0f, pTerrain->GetLength() * 0.5f)); // 여기 초기위치
+	SetPosition(XMFLOAT3(pTerrain->GetWidth() * 0.5f, fHeight + TANK_HEIGHT, pTerrain->GetLength() * 0.5f)); // 여기 초기위치
 	SetPlayerUpdatedContext(pTerrain);
 	SetCameraUpdatedContext(pTerrain);
 
